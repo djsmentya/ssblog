915c09a83c58b58263ec681b4148a6a77f0cfb0b
Eugene Smentyna (djsmentya) <djsmentya@gmail.com>
2013-06-06 16:08:14 +0300

    add hamk engine

--- config/application.rb
+++ config/application.rb
@@ -58,6 +58,11 @@ module Blog
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
     config.assets.initialize_on_precompile=false
+
+    config.generators do |g|
+      g.template_engine :haml
+    end
   end
 end
